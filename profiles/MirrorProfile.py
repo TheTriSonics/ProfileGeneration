a04820de2a5c738d6ca8b31264a4c86c39332d28
Carl Slezak <carl.slezak2@gmail.com>
2018-02-05 20:31:22 -0500

    updated the profile mirror program

--- profiles/MirrorProfile.py
+++ profiles/MirrorProfile.py
@@ -1,8 +1,10 @@
 import sys
 import fnmatch
-
+import csv
 
 print sys.argv
+
+TopToBottom = 301.72 + 50.38 - 32.5
 
 if fnmatch.filter(sys.argv,"file=*") != []:
     profile = fnmatch.filter(sys.argv,"file=*")
@@ -10,6 +12,8 @@ if fnmatch.filter(sys.argv,"file=*") != []:
 else:
     print "Optional command line argument:  file=<PROFILE_NAME>"
     profile = raw_input("Please specify a profile: ")
+
+side = raw_input("0: top\n1: middle\n2: bottom\n")
 
 profile1 = str(profile.rsplit(".")[0])
 profile = str(profile + ".csv")
@@ -23,9 +27,16 @@ with open(profile,"r") as profileFile:
     profileFileData = profileFile.read()
     profileArray = []
     profileArray = profileFileData.splitlines()
+    index = []
     for x in profileArray:
-        profileArray[profileArray.index(x)] = map(float,x.split(","))
-        profileArray[profileArray.index(map(float,x.split(",")))][1] *= -1
+        index = profileArray.index(x)
+        profileArray[index] = map(float,x.split(","))
+        profileArray[index][1] *= -1
+        print (int(side)==2)
+        if int(side) == 0:
+            profileArray[index][1] -= TopToBottom
+        elif int(side) == 2:
+            profileArray[index][1] += TopToBottom
     with open(str(profile1) + "-inverted.csv","w") as invertedProfileFile:
         profileCsv = []
         for x in profileArray:
